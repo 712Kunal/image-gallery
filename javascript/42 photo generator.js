450610877f3334c490d0ec425f2938dad9f1779d
712Kunal <jadhavkun0@gmail.com>
2024-05-02 00:22:40 +0530

    added the extra files

--- javascript/42 photo generator.js
+++ javascript/42 photo generator.js
@@ -4,6 +4,7 @@ const searchInput = document.querySelector(".input-text");
 const lightBox = document.querySelector(".lightbox");
 const closeButton = document.querySelector(".fa-circle-xmark");
 const downloadButton=document.querySelector(".fa-cloud-arrow-down");
+const dotenv=require("dotenv").config();
 
 
 const perPage = 15;   //Number of results per page
@@ -11,7 +12,7 @@ let currentPage = 1;  //page number
 let SearchTerm = null;
 
 let url = `https://api.pexels.com/v1/curated?page=${currentPage}per_page=${perPage}`;
-const apikey = "FTBOISczTCjYyAhdudpAF4s1bleQQu0JBOo9Wo3URKyIr2RH5DHoORZP";
+const apikey = process.env.MY_API_TOKEN;
 
 
 window.addEventListener('load', () => fetchPhotoes(url));
